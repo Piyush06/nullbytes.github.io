43ea15ec70a4bf373616535112c84cb5accd6252
Piyush <thakarepiyush06@gmail.com>
2017-03-16 19:45:52 +0530

    another Update

--- sendemail.php
+++ sendemail.php
@@ -1,9 +1,9 @@
 <?php
-$name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email'])); 
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'email@email.com';//replace with your email
+$name       = @trim(stripslashes($_POST['name']));
+$from       = @trim(stripslashes($_POST['email']));
+$subject    = @trim(stripslashes($_POST['subject']));
+$message    = @trim(stripslashes($_POST['message']));
+$to   		= 'thakarepiyush06@gmail.com';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
